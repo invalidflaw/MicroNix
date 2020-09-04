8e9bb1d050ff1d01edf2cbd97354f6c43d05b87e
invalidflaw <36581852+invalidflaw@users.noreply.github.com>
2020-09-04 18:55:45 -0500

    create led setting enums

--- MicroNix/ledManager.h
+++ MicroNix/ledManager.h
@@ -1,12 +1,39 @@
 #pragma once
 #include "main.h"
 
+enum class ledColor{
+  red,
+  blue,
+  green,
+  yellow,
+  aqua,
+  purple,
+  cycle,
+  rainbow,
+  juggle,
+  white
+};
+
+enum class ledPallete{
+  defaultpallete,
+  custom
+};
+
+enum class ledEffect{
+  glitter,
+  pulse,
+  bpm,
+  confetti
+};
+
 class ledManager
 {
   public:
   ledManager();
 
   void update();
+  void setMode(ledColor color);
+  
 
   private:
 };
