9cbe5e8fc9cca1da6693ccfb695fdd37ebe320db
invalidflaw <36581852+invalidflaw@users.noreply.github.com>
2020-12-22 15:37:52 -0600

    Add NTP Time to ESP32

--- MicroNix/esp_nixieTime/esp_nixieTime.ino
+++ MicroNix/esp_nixieTime/esp_nixieTime.ino
@@ -1,19 +1,27 @@
 #include <SPIFFS.h>
 #include <WiFiSettings.h>
 #include <HTTPClient.h>
+#include <NTPClient_Generic.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
+
+#if !( defined(ESP8266) ||  defined(ESP32) )
+  #error This code is intended to run on the ESP32 platform! Please check your Tools->Board setting.
+#endif
+
+#define TIME_ZONE_OFFSET_HRS            (-6)
 
 const String apiKey = "&APPID=1f217b9e242a7ee3867adb9b4bae55b2";
 const String apiCall = "http://api.openweathermap.org/data/2.5/weather?q=";
 
-String locationData;
-String weatherData;
-int checkTime = 60000;
+String locationData;  // openweathermap location string
+String weatherData;   // openweathermap response string
+int checkTime = 60000;// time between weather checks,(unused at current)
 
-// TEST strings
-char txMsg[] = "<stringtest>\n";
-char rxMsg[20] = "";
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
-TaskHandle_t weatherTask;
+DynamicJsonDocument doc(1024);
 
 void setup() {
   Serial.begin(115200);
@@ -27,27 +35,40 @@ void setup() {
   locationData = WiFiSettings.string("time_location","{city name},{state code}");
   checkTime = 60000 * WiFiSettings.integer("check interval(min)", 10);// String name, [long min, long max,] int init = 0, String label = name
   WiFiSettings.connect();
+
+  checkWeatherOnline();
+  deserializeJson(doc, weatherData);
+  int timeZone = doc["timezone"];
   
-  xTaskCreatePinnedToCore(
-    weatherGet,
-    "weather Get",
-    2000,
-    NULL,
-    1,
-    &weatherTask,
-    0
-  );    
+  timeClient.begin();
+  timeClient.setTimeOffset(timeZone);
+  timeClient.setUpdateInterval(SECS_IN_HR);
+     
 }
 
 void loop() 
-{
+{ 
   //check for a serial message from the SAMD21
   if(Serial.available())
   {
+
     String message = Serial.readStringUntil('>');
     if(message == "getweather")
     {
-      Serial.print(weatherData + ">");
+      if(WiFi.status() == WL_CONNECTED)
+      {
+        checkWeatherOnline();
+        Serial.print(weatherData + '>'); 
+      }
+      
+    }
+    else if(message == "gettime")
+    {
+      timeClient.update();
+      if(timeClient.updated())
+      {
+        Serial.print(timeClient.getEpochTime() + '>');
+      }
     }
     else if(message == "portal")
     {
@@ -57,24 +78,17 @@ void loop()
   delay(100);
 }
 
-void weatherGet(void * parameter)
+void checkWeatherOnline()
 {
-  static unsigned long lastCheck = 90000000000;
-  while(true)
-  {
-    unsigned long curTime = millis();
-    if ((WiFi.status() == WL_CONNECTED) && abs(curTime - lastCheck) > checkTime)
-    {
-//      Serial.println(apiCall + locationData + apiKey);
-      HTTPClient http;
-      http.begin(apiCall + locationData + apiKey);
-      int httpCode = http.GET();  //Make the request
-      if (httpCode > 0) { //Check for the returning code
-        weatherData = http.getString();
+  if(WiFi.status() == WL_CONNECTED)
+      {
+        HTTPClient http;
+        http.begin(apiCall + locationData + apiKey);
+        int httpCode = http.GET();  //Make the request
+        if (httpCode > 0) //Check for the returning code
+        { 
+          weatherData = http.getString();
+        }
+        http.end();
       }
-      http.end();
-      lastCheck = curTime;
-    }
-    delay(1000);    
-  }
 }
